67546a7dbfdf2fd205746b5bd9c69ff80d1cd114
Thiago Marques <thiagomarquessilva99@gmail.com>
2024-12-10 11:57:51 -0300

    Adicionando mensagens de erro

--- resources/js/Pages/Branches/ListStock.vue
+++ resources/js/Pages/Branches/ListStock.vue
@@ -29,11 +29,13 @@
                 <div class="mb-5">
                     <label for="product-name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome do Produto</label>
                     <input v-model="form_AddProduct.name" type="text" id="product-name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <span v-if="errors.name" class="text-sm text-red-700">{{ errors.name }}</span>
                 </div>
 
                 <div class="mb-5">
                     <label for="product-amount" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quantidade (apenas números)</label>
                     <input v-model="form_AddProduct.amount" type="text" id="product-amount" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    <span v-if="errors.amount" class="text-sm text-red-700">{{ errors.amount }}</span>
                 </div>
 
                 <button type="submit" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Cadastrar</button>
@@ -43,7 +45,7 @@
 
         <hr class="my-5">
 
-        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg mb-5">
             <table class="w-full text-sm text-center rtl:text-right text-gray-500 dark:text-gray-400 ">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
@@ -102,7 +104,7 @@
     import { reactive } from 'vue'
     import { router } from '@inertiajs/vue3'
 
-    const props = defineProps({ stock: Object, branch_id: Number, flash: Object });
+    const props = defineProps({ stock: Object, branch_id: Number, flash: Object, errors: Object });
 
     const form_AddProduct = reactive({
         name: null,
